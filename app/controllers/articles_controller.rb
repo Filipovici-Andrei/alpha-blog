815a12ca7aafe231636c5e3da9f571043d70f451
Filipovici-Andrei <afilipovici@yahoo.com>
2022-02-07 13:56:40 +0200

    implemented article update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
-      flash[:notice] = 'Article was created successfully. '
+      flash[:notice] = 'Article was created successfully.'
       redirect_to @article
     else
       render 'new'
@@ -19,5 +19,19 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
